d4a6a192101fc14747e4a3b8627d57ebd594673d
onnyx1 <spencer.retcher1@gmail.com>
2022-08-02 01:05:09 -0400

    Finished Game Logic

--- script.js
+++ script.js
@@ -1,22 +1,76 @@
+let containerGrid = document.querySelector(".container-grid");
+const slider = document.querySelector("#slider");
+const label = document.querySelector("#sliderLabel");
+let color = document.querySelector("#color");
 
-const containerGrid = document.querySelector(".container-grid");
+let mouseDown = false;
+
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 
-for(let i = 0; i < 16; i++){
 
+
+slider.addEventListener("input", function () {
+  changeGrid(slider.value);
+});
+color.addEventListener("input", function () {
+    hover();
+  });
+  
+
+
+
+function hover(){
+    
+    let style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = `
+    .square:hover {
+        background-color:` + color.value + `
+    }
+;`
+    
+    document.head.appendChild(style);
+}
+
+function changeGrid() {
+  label.textContent = slider.value + " x " + slider.value;
+  containerGrid.innerHTML = "";
+  containerGrid = createGrid(slider.value);
+}
+
+function createGrid(num) {
+  for (let i = 0; i < num; i++) {
     const div = document.createElement("div");
     div.classList.add("row");
 
-    for(let i = 0; i < 16; i++){
-
-    const square = document.createElement("div");
-    square.classList.add("square");
-    div.appendChild(square);
-
+    for (let i = 0; i < num; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      div.appendChild(square);
     }
 
+    containerGrid.appendChild(div);
+  }
 
-containerGrid.appendChild(div);
+  const squares = document.querySelectorAll(".square");
+
+  for (let square of squares) {
+    square.addEventListener("mouseenter", function (event) {
+      if (mouseDown === true) {
+        square.style.backgroundColor = color.value;
+      }
+    });
+
+    square.addEventListener("click", function (event) {
+      square.style.backgroundColor = color.value;
+    });
 
 
+  }
+
+  hover();
+
+  return containerGrid;
 }
